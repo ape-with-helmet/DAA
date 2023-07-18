19d60b3b1c34eed40a072e0d665d5e1aa370bb81
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-07-18 10:00:20 +0000

    ....

--- dijkstra.py
+++ dijkstra.py
@@ -5,7 +5,6 @@ def dijkstra(graph,start):
     distances={node: float('inf') for node in graph}
     distances[start]=0
     heap=[(0,start)]
-
     while heap:
         current_dist,current_node=heapq.heappop(heap)
         if current_dist>distances[current_node]:
@@ -23,7 +22,6 @@ def find_optimal_route(graph,start,destination):
         return None
     route=[]
     node=destination
-
     while node!=start:
         route.append(node)
         neighbors=graph[node]
@@ -33,16 +31,23 @@ def find_optimal_route(graph,start,destination):
             if distances[neighbor]+weight==distances[node]and distances[neighbor]<min_distance:
                 min_distance=distances[neighbor]
                 next_node=neighbor
-        if next_node id None or next_node in route:
+        if next_node is None or next_node in route:
+            print(next_node,distances)
             return None
         node=next_node
-    
     route.append(start)
     route.reverse()
     return route
-
-start_location='A'
-destination_location='E'
+#graph[]
+graph={
+    'A':{'B':3,'C':99,'D':7,'E':99},
+    'B':{'A':3,'C':99,'D':7,'E':99},
+    'C':{'A':99,'C':99,'D':7,'E':99},
+    'D':{'A':7,'C':99,'D':7,'E':99},
+    'E':{'A':3,'C':99,'D':7,'E':99},
+}
+start_location='C'
+destination_location='A'
 optimal_route=find_optimal_route(graph,start_location,destination_location)
 if optimal_route is None:
     print("No valid route")
