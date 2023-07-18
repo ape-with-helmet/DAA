9d5bc4a4323f3252ec84c1f2280a916a6022d36a
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-07-18 11:12:49 +0000

    ....

--- huffmanalgorithm.py
+++ huffmanalgorithm.py
@@ -1,64 +1,81 @@
-'''Imports heap.'''
 import heapq
-
-
 class Node:
-    '''Starts node.'''
-
-    def __init__(self, f_y, symbol, l_t=None, r_t=None):
-        # frequency of symbol
-        self.freq = f_y
-        # symbol name(character)
-        self.symbol = symbol
-        # node left of current node
-        self.left = l_t
-        # node right of current node
-        self.right = r_t
-        # tree direction(0/1)
-        self.huff = ' '
-
+    def __init__(self, freq, symbol, left=None, right=None):
+        self.freq=freq
+        self.symbol=symbol
+        self.left=left
+        self.right=right
+        self.huff=''
     def __lt__(self, nxt):
-        return self.freq < nxt.freq
-# utility function.
-
-
-def print_nodes(node, val=''):
-    '''Printz.'''
-    newval = val + str(node.huff)
-    if node.left:
+        return self.freq< nxt.freq
+def print_nodes(node,val=''):
+    newval= val + str(node.huff)
+    if(node.left):
         print_nodes(node.left, newval)
-    if node.right:
+    if(node.right):
         print_nodes(node.right, newval)
-    if not node.left and not node.right:
-        print(f"{node.symbol}->{newval}")
-
-#n=int(input("Enter the no of characters: "))
+    if(not node.left and not node.right):
+        print(f"{node.symbol} -> {newval}")
+chars=[]
+n=int(input("Enter the nodes:"))
+for i in range(0,n):
+    l=input(f"Node {i+1}:")
+    chars.append(l)
+freq=[]
+print("Enter the frequencies:\n")
+for i in range(0,n):
+    m=int(input(f"frequency of node {i+1}:"))
+    freq.append(m)
 nodes=[]
-chars=input('').split(" ")
-freq=[int(i) for i in input('').split(" ")]
 for x in range(len(chars)):
-    heapq.heappush(nodes, Node(freq[x], chars[x]))
-while len(nodes) > 1:
-    left = heapq.heappop(nodes)
-    right = heapq.heappop(nodes)
-    left.huff = 0
-    right.huff = 1
-    SY=str(left.symbol+right.symbol)
-    FR=left.freq+right.freq
-    newNode = Node(FR, SY, left, right)
+    heapq.heappush(nodes, Node(freq[x],chars[x]))
+
+while len(nodes)>1:
+    left=heapq.heappop(nodes)
+    right=heapq.heappop(nodes)
+    left.huff=0
+    right.huff=1
+    newNode=Node(left.freq+right.freq, left.symbol+right.symbol, left, right)
     heapq.heappush(nodes, newNode)
 print_nodes(nodes[0])
+def decode_text(encoded_text, starting_node):
+    decoded_text = ''
+    current_node = starting_node
 
-#inputs
-#a  b  c  d e f  g
-#10 15 12 3 4 13 1
+    for bit in encoded_text:
+        if bit == '0':
+            current_node = current_node.left
+        else:
+            current_node = current_node.right
 
-#a b c d e f g
-#10 15 12 3 4 13 1
-#c-> 00
-#f-> 01
-#b-> 10
-#e-> 1100
-#g-> 11010
-#d-> 11011
-#a-> 111
+        if not current_node.left and not current_node.right:
+            decoded_text += current_node.symbol
+            current_node = starting_node
+
+    return decoded_text
+
+starting_node = nodes[0]
+encoded_text = input("Enter the encoded text: ")
+decoded_text = decode_text(encoded_text, starting_node)
+print("Decoded text:", decoded_text)
+def encode_text(text, starting_node):
+    encoding_map = {}
+
+    def traverse_tree(node, code=''):
+        if node.left:
+            traverse_tree(node.left, code + '0')
+        if node.right:
+            traverse_tree(node.right, code + '1')
+        if not node.left and not node.right:
+            encoding_map[node.symbol] = code
+
+    traverse_tree(starting_node)
+
+    encoded_text = ''
+    for char in text:
+        encoded_text += encoding_map[char]
+
+    return encoded_text
+text_input = input("Enter text to encode: ")
+encoded_binary_text = encode_text(text_input, starting_node)
+print("Encoded binary text:", encoded_binary_text)
