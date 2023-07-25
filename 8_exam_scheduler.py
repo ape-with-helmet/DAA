618ae5e7c396b38cd951f3abf22b9bff9b59420c
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-07-25 10:34:37 +0000

    psjofi

--- 8_exam_scheduler.py
+++ 8_exam_scheduler.py
@@ -12,10 +12,36 @@ class Graph:
         self.graph[subject1].append(subject2)
         self.graph[subject2].append(subject1)
     def graph_coloring(self):
-        color map={}
+        color_map={}
         available_colors=set(range(1,len(self.subjects)+1))
         for subject in self.subjects:
-            used colors = set()
+            used_colors = set()
             for neighbor in self.graph[subject]:
-                if neighbor in color map:
-                    
+                if neighbor in color_map:
+                    used_colors.add(color_map[neighbor])
+                available_color=available_colors-used_colors
+                if available_color:
+                    color_map[subject]=min(available_color)
+                else:
+                    color_map[subject]=len(available_colors)+1
+                    available_colors.add(color_map[subject])
+        return color_map
+    def get_minimum_time_slots(self):
+        color_map=self.graph_coloring()
+        return max(color_map.values())
+
+#Example usage
+subjects=['math','phy','chem','bio']
+students = {
+    'math':['alice','bob','charlie'],
+    'phy':['alice','charlie','david'],
+    'chem':['bob','charlie','eve'],
+    'bio':['alice','david','eve']
+}
+graph=Graph(subjects)
+graph.add_edge('math','phy')
+graph.add_edge('math','chem')
+graph.add_edge('phy','chem')
+graph.add_edge('phy','bio')
+minimum_time_slots=graph.get_minimum_time_slots()
+print(f"Minimum time slots needed: {minimum_time_slots}")
